6acf7e41519c1aa7e09fa8e6e3632f8e7b218ed5
ray <ray@boyamarine.com>
2018-02-24 13:41:46 +0800

    修改pom.xml中spring-boot-maven-plugin的配置位置，修复mvn打包时出现的classnotfound错误

--- bycrm-web/src/main/java/org/boya/platform/bycrm/BycrmApplication.java
+++ bycrm-web/src/main/java/org/boya/platform/bycrm/BycrmApplication.java
@@ -1,16 +1,32 @@
 package org.boya.platform.bycrm;
 
+import org.hswebframework.web.authorization.listener.event.AuthorizingHandleBeforeEvent;
+import org.hswebframework.web.loggin.aop.EnableAccessLogger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableCaching //开启缓存
+@EnableAccessLogger //开启访问日志
+@ComponentScan("org.boya.platform.bycrm")
 @MapperScan(basePackages = "org.boya.platform.bycrm.impl")
-public class BycrmApplication {
+public class BycrmApplication implements ApplicationListener<AuthorizingHandleBeforeEvent> {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BycrmApplication.class, args);
+	}
+
+	@Override
+	public void onApplicationEvent(AuthorizingHandleBeforeEvent event) {
+		//admin 拥有所有权限
+		if(event.getContext().getAuthentication().getUser().getUsername().equals("admin")){
+			event.setAllow(true);
+		}
 	}
 }
